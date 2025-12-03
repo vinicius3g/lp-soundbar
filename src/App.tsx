e3ea68f5fb3ebc1804a6b44929ee330641112da2
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2025-12-03 01:03:43 -0300

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.scss";
+import { Hero } from "./components/Hero/Hero";
+import { Section } from "./components/Section";
+import { FeaturesGrid } from "./components/FeaturesGrid";
+import { QualitySection } from "./components/QualitySection";
+import { WirelessSection } from "./components/WirelessSection";
+import { WarrantySection } from "./components/WarrantySection";
+import { PRODUCT_FEATURES } from "./utils";
+import { ConnectivitySection } from "./components/ConnectivitySection";
+import { InstallationSection } from "./components/InstallationSection";
 
-function App() {
+import soundbar from "./assets/images/soundbar-waves.png";
+import { ImageContent } from "./components/ImageContent";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="app">
+      <Hero />
+      <Section>
+        <p className="intro-text">
+          Com a Soundbar com <span className="emphasis-text">Subwoofer Wireless Pulse</span> Subwoofer
+          Wireless Pulse, você vai levar para a sua sala o impactante som do
+          cinema de forma prática, sem ocupar espaço e sem cabos. Reúna a
+          família e amigos e desfrute!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Section>
+
+      <Section title="Soundbar com Subwoofer Wireless Pulse" dark={true}>
+        <p className="wireless-text">Transforme um simples som em uma potência.</p>
+      </Section>
+
+      <Section>
+        <p className="section-text-third">
+          A Soundbar e o Subwoofer Wireless Pulse oferecem, juntos, uma
+          experiência de áudio profunda e poderosa. Um efeito que envolve todo o
+          ambiente!
+        </p>
+
+        <p className="section-text-third__subtext">
+          Sem complicações! Conecte sua Soundbar com Subwoofer Wireless Pulse,
+          por meio das conexões bluetooth, auxiliar, coaxial, óptica e HDMI ARC,
+          para assistir filmes, séries e outros tipos de entretenimento ou
+          escute suas músicas preferidas pela porta USB. A Soundbar com
+          Subwoofer Wireless Pulse é discreta, moderna e combina com qualquer
+          decoração. Pulse"
+        </p>
+      </Section>
+
+      <FeaturesGrid features={PRODUCT_FEATURES} />
+      <QualitySection />
+
+      <section className="middle_banner_section">
+        <ImageContent
+          className="middle_banner"
+          imageAlt="spund-bar image"
+          imageUrl={soundbar}
+          isBannerFull
+        />
+      </section>
+
+      <WirelessSection />
+      <ConnectivitySection />
+      <InstallationSection />
+      <WarrantySection />
     </div>
   );
-}
+};
 
 export default App;
